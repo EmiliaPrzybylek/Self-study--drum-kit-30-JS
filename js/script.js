64246459379a04b9fdca805a1d6c35dd95d04fd3
Emilia <emiliaprzybylek@tutamail.com>
2022-05-25 11:45:46 +0200

    ja: click event added

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
 'use strict';
 
+
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    let code;
+    if (e.keyCode) {
+      // it was a keypress, get the keycode as usual
+      code = e.keyCode;
+    } else {
+      // it was a click.  Read the keycode from the div that was clicked
+      code = this.dataset.key;
+    }
+  
+    const audio = document.querySelector(`audio[data-key="${code}"]`);
+    const key = document.querySelector(`.key[data-key="${code}"]`);
+  
     if(!audio) return;
     audio.currentTime = 0;
     audio.play();
-    key.classList.add('playing')
-}
+    key.classList.add('playing');
+  }
+    function removeTransition(e) {
+        if(e.propertyName !== 'box-shadow') return;
+        this.classList.remove('playing');
+    }
 
-function removeTransition(e) {
-    if(e.propertyName !== 'box-shadow') return;
-    this.classList.remove('playing');
-}
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+for (let i=0, key; key = keys[i]; i++) {
+    key.addEventListener('transitionend', removeTransition);
+    key.addEventListener('click', playSound);
+  }
 
 window.addEventListener('keydown', playSound);
-keys.addEventListener('click', playSound)
+
