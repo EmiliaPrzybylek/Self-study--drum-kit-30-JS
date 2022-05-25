06a8da29651484735b5bc1c12a9f728ba115a386
Emilia <emiliaprzybylek@tutamail.com>
2022-05-25 11:49:31 +0200

    html: .text corrected

--- js/script.js
+++ js/script.js
@@ -1,35 +1,31 @@
-'use strict';
-
+"use strict";
 
 function playSound(e) {
-    let code;
-    if (e.keyCode) {
-      // it was a keypress, get the keycode as usual
-      code = e.keyCode;
-    } else {
-      // it was a click.  Read the keycode from the div that was clicked
-      code = this.dataset.key;
-    }
-  
-    const audio = document.querySelector(`audio[data-key="${code}"]`);
-    const key = document.querySelector(`.key[data-key="${code}"]`);
-  
-    if(!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-  }
-    function removeTransition(e) {
-        if(e.propertyName !== 'box-shadow') return;
-        this.classList.remove('playing');
-    }
-
-
-const keys = document.querySelectorAll('.key');
-for (let i=0, key; key = keys[i]; i++) {
-    key.addEventListener('transitionend', removeTransition);
-    key.addEventListener('click', playSound);
+  let code;
+  if (e.keyCode) {
+    code = e.keyCode;
+  } else {
+    code = this.dataset.key;
   }
 
-window.addEventListener('keydown', playSound);
+  const audio = document.querySelector(`audio[data-key="${code}"]`);
+  const key = document.querySelector(`.key[data-key="${code}"]`);
 
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "box-shadow") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+for (let i = 0, key; (key = keys[i]); i++) {
+  key.addEventListener("transitionend", removeTransition);
+  key.addEventListener("click", playSound);
+}
+
+window.addEventListener("keydown", playSound);
